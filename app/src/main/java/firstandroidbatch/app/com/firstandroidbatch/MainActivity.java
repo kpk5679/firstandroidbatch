8da2c7a8c314ddd55e284ec9606e9bb0d45f6c51
Praveen Karkala Kini <Praveen.Kini@jda.com>
2016-11-24 10:31:41 +0530

    Reverted back to the google working page.
    https://developer.android.com/training/basics/firstapp/starting-activity.html
    
    Committed for Praveen Karkala Kini

--- app/src/main/java/firstandroidbatch/app/com/firstandroidbatch/MainActivity.java
+++ app/src/main/java/firstandroidbatch/app/com/firstandroidbatch/MainActivity.java
@@ -11,7 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +43,6 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-    /** Called when the user clicks the Send button */
-    public void displayTime(View view) {
-        Intent intent = new Intent(this, DateTimeActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
